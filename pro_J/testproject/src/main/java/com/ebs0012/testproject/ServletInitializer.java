283e7b2cced6f092c0fee09248d6873df709668c
BSEom <ebs970725@gmail.com>
2025-05-15 15:04:14 +0900

    이전 마지막 업데이트

---
+++ pro_J/testproject/src/main/java/com/ebs0012/testproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ebs0012.testproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestprojectApplication.class);
+	}
+
+}
