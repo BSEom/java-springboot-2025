f18e83dcf80953bae53840949faf5939ce84d3cc
BSEom <ebs970725@gmail.com>
2025-07-02 09:31:55 +0900

    일차 분리

---
+++ day07/backboard/src/main/java/com/pknu/backboard/repository/ReplyRepository.java
@@ -0,0 +1,9 @@
+package com.pknu.backboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pknu.backboard.entity.Reply;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    // 내용은 필요없음. JpaRepository에 기능이 다 존재
+}
